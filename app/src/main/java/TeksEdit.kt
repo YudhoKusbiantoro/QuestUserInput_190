545a330b0e36f1b9592d6bb02793a1a26137ec70
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2025-10-31 09:18:13 +0700

    update typo mutableStateof

--- app/src/main/java/TeksEdit.kt
+++ app/src/main/java/TeksEdit.kt
@@ -1,19 +1,25 @@
+import android.R
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import java.lang.reflect.Modifier
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier
 ){
     // variable-variable untuk mengingat nilai masukan dari keyboard
 
-    var textNama by remember { mutableSetOf("") }
-    var textAlamat by remember { mutableSetOf("") }
-    var textJK by remember { mutableSetOf("") }
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
 
     // variable variable untuk menyimpan data yang di peroleh dari komonen UI
 
-    var nama by remember { mutableSetOf("") }
-    var alamat by remember { mutableSetOf("") }
-    var jenis by remember { mutableSetOf("") }
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+
+    val gender:List<String> = listOf("Laki-laki","Perempuan" )
 }
